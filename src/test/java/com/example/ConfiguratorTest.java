2a09e3e7e334f51ee83e98792b3375f122eecc97
Paul Dunnavant <pdunnavant@gmail.com>
2011-11-11 16:30:15 -0800

    add test for configurator

--- src/test/java/com/example/ConfiguratorTest.java
+++ src/test/java/com/example/ConfiguratorTest.java
@@ -10,23 +10,24 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class FacebookTest extends CamelSpringTestSupport {
+public class ConfiguratorTest extends CamelSpringTestSupport {
 
 	@Override
 	protected AbstractApplicationContext createApplicationContext() {
-		return new ClassPathXmlApplicationContext("facebook.xml");
+		return new ClassPathXmlApplicationContext("configurator.xml");
 	}
 
 	@Test
-	public void testFacebookRout() throws Exception {
-		// This will probably need to be updated with a token attached to the example URLs at this URL:
-		// https://developers.facebook.com/docs/reference/api/
-		String token = "AAAAAAITEghMBAIeBeE8AfcXTjqL1x4GFI03ZAll5ZBvICRPRrpCxRcz8TqhQd40hAyZBhGELzt8k6fpN8SZAPYZCw84BZCVYV3T6XBGudJ3wZDZD";
-		token = URLEncoder.encode(token, "UTF-8");
+	public void testConfiguratorRout() throws Exception {
+		String model = "a7";
+		model = URLEncoder.encode(model, "UTF-8");
+
 		MockEndpoint endPoint = getMockEndpoint("mock:finish");
 		endPoint.setExpectedMessageCount(1);
+
 		Map<String, Object> headers = new HashMap<String, Object>();
-		headers.put("AccessToken", token);
+		headers.put("Model", model);
+
 		sendBody("direct:start", null, headers);
 		endPoint.assertIsSatisfied();
 	}
